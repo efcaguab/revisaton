d78c7d7fbd562e918c9dd9709ceb30d814a04d17
Fernando Cagua <efcaguab@gmail.com>
2018-06-03 01:18:20 +1200

    update script that generates json so that it includes the metadata in json files and saves a csv with the link data

--- generate_json.R
+++ generate_json.R
@@ -4,15 +4,50 @@ out_folder <- "public_html/json-data"
 mesas_data_files <- list.files(folder, recursive = T, full.names = T)
 mesas_data_files_name_only <- list.files(folder, recursive = T, full.names = F)
 
+read_add <- function(x, col_types){
+  readr::read_csv(x, col_types = col_types) %>%
+    dplyr::mutate(path = x)
+}
+
 mesas_data <- mesas_data_files %>%
   purrr::array_branch() %>%
-  purrr::map_df(readr::read_csv, col_types = "cc")
+  purrr::map_df(read_add, col_types = "cc")
 
 mesas_data_sep <- mesas_data %>%
   dplyr::mutate(file_str = stringr::str_sub(href, 53, -1)) %>% 
   tidyr::separate(file_str, c("departamento", "municipio", "zona", "pre", "filename"), sep = "/", remove = F) %>% 
   tidyr::separate(filename, c("no", "e14", "pre", "x", "departamento2", "municipio2", "zona2", "xx", "puesto", "consecutivo", "x2", "xxx"), sep = "_") %>%
-  dplyr::distinct()
+  tidyr::separate(path, c("base_dir", "dep_path", "mun_path", "zona_name", "pto_path"), sep = "/")%>%
+  tidyr::separate(dep_path, c("departamento_path", "departamento_str_path"), sep = " - ") %>%
+  tidyr::separate(departamento_str_path, c("departamento_name_path"), sep = " \\(", extra = "drop") %>%
+  tidyr::separate(mun_path, c("municipio_path", "municipio_name_path"), sep = " - ") %>%
+  tidyr::separate(zona_name, c("x", "zona_path"), sep = " ", remove = F) %>%
+  tidyr::separate(pto_path, c("puesto_path", "puesto_name_path"), sep = " - ", extra = "merge") %>%
+  tidyr::separate(mesa_name, c("xx", "mesa"), sep = " ", extra = "merge", remove = F) %>%
+  dplyr::mutate(departamento_path = as.numeric(departamento_path), 
+                departamento_path = sprintf("%02d", departamento_path), 
+                municipio_path = as.numeric(municipio_path), 
+                municipio_path = sprintf("%03d", municipio_path), 
+                municipio_name_path = trimws(municipio_name_path),
+                zona_path = as.numeric(zona_path), 
+                zona_path = sprintf("%03d", zona_path),
+                puesto_name_path = stringr::str_sub(puesto_name_path, end = -5)) %>%
+  dplyr::filter(departamento == departamento_path, 
+                municipio == municipio_path, 
+                zona == zona_path, 
+                puesto == puesto_path) %>%
+  dplyr::mutate(d = duplicated(no, fromLast = F)) %>%
+  dplyr::filter(!d) %>%
+  dplyr::select(href, mesa_name, 
+                departamento,
+                departamento_name = departamento_name_path, 
+                municipio, municipio_name = municipio_name_path,
+                zona, zona_name,
+                puesto, puesto_name = puesto_name_path, 
+                mesa,
+                file_str, no)
+  
+readr::write_csv(mesas_data_sep, "public_html/form-data.csv")
 
 simplify <- . %>% dplyr::summarise(n = n()) %>% 
   jsonlite::toJSON(dataframe = "values") %>%
@@ -52,14 +87,18 @@ o <- mesas_data_sep %>%
 save_puesto <- function(x, out_folder){
   this_folder <- file.path(out_folder, "pto", x$departamento[1], x$municipio[1]) 
   suppressWarnings(dir.create(this_folder, recursive = T))
-  x %>% 
-    dplyr::select(file_str, no) %>%
+  df <- x %>% 
+    dplyr::select(file_str, no, puesto_name, mesa)
+  list(dep = x$departamento_name[1], mun = x$municipio_name[1], zon = x$zona_name[1],
+       df = df)%>%
     jsonlite::toJSON(dataframe = "values") %>%
     jsonlite::minify() %>%
     write(file.path(this_folder, paste0(x$zona[1], ".json")))
 }
 l <- mesas_data_sep %>%
   split(list(.$departamento, .$municipio, .$zona))
+nrowl <- l %>% purrr::map_dbl(nrow)
+l <- l[nrowl > 0]
 pb <- dplyr::progress_estimated(length(l))
 o <- l %>%
     purrr::map(~{
